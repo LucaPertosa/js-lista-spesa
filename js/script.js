b983b48cd0e37094bfb0a615d3d7892211f8c3fb
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-15 13:31:18 +0100

    Add: project scaffolding

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Creo l'array 
+const shoppingList = ['pane', 'latte', 'uova', 'burro', 'salame'];
+console.log(shoppingList);
+// prendo il riferimento all'ul
+const list = document.getElementById('shopping-list');
+console.log(list);
