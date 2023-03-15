67479e8f708145b23b7bc16c471d31263b948db9
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-15 13:40:56 +0100

    Complete lista della spesa con ciclo while

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ console.log(shoppingList);
 // prendo il riferimento all'ul
 const list = document.getElementById('shopping-list');
 console.log(list);
+
+// Inizializzo ciclo while
+let i = 0;
+// utilizzo ciclo while per creare gli elementi della lista
+while (i < shoppingList.length) {
+    // creo la costante che contiene i list item
+    const listItem = `<li>${shoppingList[i]}</li>`;
+    console.log(listItem);
+    // inseriamo per ogni ciclo i list item creato all'interno del riferimento prelevato all'inizio (riga 4-5)
+    list.innerHTML += listItem;
+    i++;
+}
